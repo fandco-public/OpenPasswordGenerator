520cc0b5b47bc90436a04452e88f28b406a917f8
Jignesh Desai <jignesh_desai@freytag.us>
2021-10-02 21:06:20 +0530

    Add project files.

---
+++ SamplePasswordGenerator/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SamplePasswordGenerator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            lblNewPassword.Text = GeneratePassword((int)this.currentSlider.Value);
+        }
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            DataPackage dataPackage = new DataPackage();
+            dataPackage.RequestedOperation = DataPackageOperation.Copy;
+            dataPackage.SetText(lblNewPassword.Text);
+
+            Clipboard.SetContent(dataPackage);
+
+
+        }
+
+        public string GeneratePassword(int length)
+        {
+
+            RNGCryptoServiceProvider cryptRNG = new RNGCryptoServiceProvider();
+            byte[] tokenBuffer = new byte[length];
+            cryptRNG.GetBytes(tokenBuffer);
+            return Convert.ToBase64String(tokenBuffer).Remove(length);
+
+        }
+
+        private void currentSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if(lblSliderValue != null)
+            lblSliderValue.Text = currentSlider.Value.ToString();
+        }
+    }
+}
