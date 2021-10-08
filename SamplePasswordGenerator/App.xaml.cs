88b69bd2cb02452580ff22aaca474772c8a20228
Jignesh Desai <jignesh_desai@freytag.us>
2021-10-08 15:41:21 +0530

    Changed Namespace using Rename feature of VS.

--- SamplePasswordGenerator/App.xaml.cs
+++ SamplePasswordGenerator/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace SamplePasswordGenerator
+namespace OpenPasswordGenerator
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
